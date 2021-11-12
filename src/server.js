1b09bf67776d2aaaf6267ed27a74d59fef03c3da
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-11-12 11:19:43 +0100

    Add files
    Initialize with files.

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+import mongoose from "mongoose";
+
+
+const server = express();
+
+// GLOBAL MIDDLEWARE
+server.use(cors());
+server.use(express.json());
+
+// ROUTES
+
+
+// ERROR-HANDLING MIDDLEWARE
+
+
+
+const { PORT } = process.env;
+
+
+mongoose.connect(process.env.MONGODB_URI); // NEED ONE MONGO DB CONNECTION FROM SOMEONE.
+mongoose.connection.on(connected, () => {
+server.listen(PORT, () => {
+    console.table(listEndpoints(server));
+    console.log("Server is running on port:", PORT);
+  });
+});
